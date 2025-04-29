754b65de573e360084528c69498939bf7fd01b4f
Scott Rayapoullé <scott.rayapoulle@adevinta.com>
2025-04-29 13:37:28 +0200

    Add missing import for `LocalClipboard`

--- catalog/src/main/kotlin/com/adevinta/spark/catalog/icons/IconsScreen.kt
+++ catalog/src/main/kotlin/com/adevinta/spark/catalog/icons/IconsScreen.kt
@@ -57,6 +57,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalClipboard
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.semantics
