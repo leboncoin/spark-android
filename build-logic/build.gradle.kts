0899c70d565151e1e23aad06632235e26d89c8e8
Simon Marquis <simon.marquis@adevinta.com>
2025-07-03 08:35:53 +0100

    chore: rewrite Gradle plugins dependencies in build-logic (#1614)

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -42,17 +42,26 @@ kotlin {
 repositories {
     google()
     mavenCentral()
+    gradlePluginPortal()
 }
 
 dependencies {
+    fun DependencyHandlerScope.plugin(plugin: Provider<PluginDependency>) = plugin.map {
+        "${it.pluginId}:${it.pluginId}.gradle.plugin:${it.version.requiredVersion}"
+    }
     compileOnly(gradleApi())
-    compileOnly(libs.gradlePlugins.android)
-    compileOnly(libs.gradlePlugins.kotlin)
-    compileOnly(libs.gradlePlugins.compose)
-    compileOnly(libs.gradlePlugins.dependencyGuard)
-    compileOnly(libs.gradlePlugins.dokka)
-    compileOnly(libs.gradlePlugins.spotless)
-    compileOnly(libs.gradlePlugins.nmcp)
+    implementation(plugin(libs.plugins.android.application))
+    implementation(plugin(libs.plugins.android.kotlin))
+    implementation(plugin(libs.plugins.android.library))
+    implementation(plugin(libs.plugins.android.lint))
+    implementation(plugin(libs.plugins.android.test))
+    implementation(plugin(libs.plugins.compose))
+    implementation(plugin(libs.plugins.dependencyGuard))
+    implementation(plugin(libs.plugins.dokka))
+    implementation(plugin(libs.plugins.kotlin.jvm))
+    implementation(plugin(libs.plugins.nmcp))
+    implementation(plugin(libs.plugins.paparazzi))
+    implementation(plugin(libs.plugins.spotless))
     implementation(libs.dokka.base)
 }
 
