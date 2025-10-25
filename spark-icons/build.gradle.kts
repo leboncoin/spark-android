953f9abfec96e357b65d74b68041739ba4f5d164
Simon Marquis <simon.marquis@adevinta.com>
2025-10-25 15:40:23 +0200

    feat: add circular dependency on BoM
    Added dependency for spark.bom platform.

--- spark-icons/build.gradle.kts
+++ spark-icons/build.gradle.kts
@@ -39,6 +39,7 @@ kotlin {
 }
 
 dependencies {
+    api(platform(projects.spark.bom)))
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.appCompat.resources) // Needed for compat vector drawables
     api(libs.androidx.compose.animation.core) {
