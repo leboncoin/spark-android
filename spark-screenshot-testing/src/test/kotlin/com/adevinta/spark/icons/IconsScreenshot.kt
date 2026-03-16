26b9cde2e5d14595839fe83c9ea54618ae57748b
Scott Rayapoullé <scott.rayapoulle.ext@adevinta.com>
2026-03-16 18:13:26 +0100

    Use the correct LeboncoinIcons name for LeboncoinIcons for android sourceSet
    Signed-off-by: Scott Rayapoullé <scott.rayapoulle.ext@adevinta.com>

--- spark-screenshot-testing/src/test/kotlin/com/adevinta/spark/icons/IconsScreenshot.kt
+++ spark-screenshot-testing/src/test/kotlin/com/adevinta/spark/icons/IconsScreenshot.kt
@@ -40,7 +40,7 @@ internal class IconsScreenshot {
     val paparazzi = paparazziRule()
 
     internal object SparkIconProvider : TestParameterValuesProvider() {
-        override fun provideValues(context: Context) = Class.forName("com.adevinta.spark.icons.LeboncoinIconsKt")
+        override fun provideValues(context: Context) = Class.forName("com.adevinta.spark.icons.LeboncoinIcons_androidKt")
             .methods
             .filter { SparkIcon::class.java.isAssignableFrom(it.returnType) }
             .map { value(it.invoke(null, LeboncoinIcons) as SparkIcon).withName(it.name.removePrefix("get")) }
@@ -50,12 +50,14 @@ internal class IconsScreenshot {
     @Suppress("JUnitMalformedDeclaration")
     fun render(
         @TestParameter(valuesProvider = SparkIconProvider::class) icon: SparkIcon,
-    ) = paparazzi.sparkSnapshot(drawBackground = false) {
-        Icon(
-            sparkIcon = icon,
-            contentDescription = icon.toString(),
-            size = IconSize.ExtraLarge,
-            tint = Color.Black,
-        )
+    ) {
+        paparazzi.sparkSnapshot(drawBackground = false) {
+            Icon(
+                sparkIcon = icon,
+                contentDescription = icon.toString(),
+                size = IconSize.ExtraLarge,
+                tint = Color.Black,
+            )
+        }
     }
 }
