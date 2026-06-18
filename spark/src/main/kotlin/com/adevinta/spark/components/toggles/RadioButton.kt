469ede2579a6bd5a696fbfc4013096df0dc8570e
Scott Rayapoullé <scott.rayapoulle@adevinta.com>
2026-06-18 11:27:01 +0200

    chore(toggles)!: remove apis for content-side and deprecated intents (#2032)
    Co-authored-by: spark-ui-bot <spark-ui-bot@users.noreply.github.com>
    Signed-off-by: Scott Rayapoullé <scott.rayapoulle@adevinta.com>

--- spark/src/main/kotlin/com/adevinta/spark/components/toggles/RadioButton.kt
+++ spark/src/main/kotlin/com/adevinta/spark/components/toggles/RadioButton.kt
@@ -38,6 +38,7 @@ import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import com.adevinta.spark.InternalSparkApi
 import com.adevinta.spark.PreviewTheme
+import com.adevinta.spark.components.IntentColors
 import com.adevinta.spark.components.text.Text
 import com.adevinta.spark.tools.modifiers.sparkUsageOverlay
 
@@ -48,7 +49,7 @@ internal fun SparkRadioButton(
     selected: Boolean,
     onClick: (() -> Unit)?,
     modifier: Modifier = Modifier,
-    intent: ToggleIntent = ToggleIntent.Support,
+    intent: IntentColors = IntentColors.Support,
     enabled: Boolean = true,
     interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
 ) {
@@ -59,55 +60,6 @@ internal fun SparkRadioButton(
         enabled = enabled,
         colors = intent.toRadioButtonDefaultsColors(),
         modifier = modifier.sparkUsageOverlay(),
-    )
-}
-
-/**
- *
- * Radio buttons allow users to select one option from a set.
- *
- *  - Use radio buttons to select a single option from a list
- *  - It should be visible at a glance if a radio button has been selected, and selected items should be more visually
- *  prominent than unselected items.
- *  - Present a list showing all available options. If available options can be collapsed, consider using a dropdown
- *  menu because it uses less space.
- *
- * @see [SparkRadioButton] if you require color customization between states. Be aware that this is still an internal
- * composable so if you need such state contact the Spark team
- *
- * @param selected whether this radio button is selected or not
- * @param onClick callback to be invoked when the RadioButton is clicked. If null, then this RadioButton will not
- * handle input events, and only act as a visual indicator of selected state
- * @param modifier Modifier to be applied to the layout of the radiobutton
- * @param intent The [ToggleIntent] to use to draw the radio button
- * @param enabled whether the component is enabled or grayed out
- * @param interactionSource the [MutableInteractionSource] representing the stream of
- * [Interaction]s for this RadioButton. You can create and pass in your own remembered
- * [MutableInteractionSource] if you want to observe [Interaction]s and customize the
- * appearance / behavior of this RadioButton in different [Interaction]s.
- */
-@Deprecated(
-    message = "Intent RadioButton have been deprecated in favour of just an error parameter",
-    replaceWith = ReplaceWith(
-        "RadioButton(selected = selected, onClick = onClick, modifier = modifier, enabled = enabled, error = false, interactionSource = interactionSource)",
-    ),
-)
-@Composable
-public fun RadioButton(
-    selected: Boolean,
-    onClick: (() -> Unit)?,
-    intent: ToggleIntent,
-    modifier: Modifier = Modifier,
-    enabled: Boolean = true,
-    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
-) {
-    SparkRadioButton(
-        intent = intent,
-        selected = selected,
-        onClick = onClick,
-        interactionSource = interactionSource,
-        enabled = enabled,
-        modifier = modifier,
     )
 }
 
@@ -147,79 +99,12 @@ public fun RadioButton(
     interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
 ) {
     SparkRadioButton(
-        intent = if (error) ToggleIntent.Danger else ToggleIntent.Support,
+        intent = if (error) IntentColors.Danger else IntentColors.Support,
         selected = selected,
         onClick = onClick,
         interactionSource = interactionSource,
         enabled = enabled,
         modifier = modifier,
-    )
-}
-
-/**
- *
- * Radio buttons allow users to select one option from a set.
- *
- *  - Use radio buttons to select a single option from a list
- *  - It should be visible at a glance if a radio button has been selected, and selected items should be more visually
- *  prominent than unselected items.
- *  - Present a list showing all available options. If available options can be collapsed, consider using a dropdown
- *  menu because it uses less space.
- *
- * @see [SparkRadioButton] if you require color customization between states. Be aware that this is still an internal
- * composable so if you need such state contact the Spark team
- *
- * @param selected whether this radio button is selected or not
- * @param onClick callback to be invoked when the RadioButton is clicked. If null, then this RadioButton will not
- * handle input events, and only act as a visual indicator of selected state
- * @param modifier Modifier to be applied to the layout of the checkbox
- * @param intent The [ToggleIntent] to use to draw the radio button
- * @param enabled whether the component is enabled or grayed out
- * @param interactionSource the [MutableInteractionSource] representing the stream of
- * [Interaction]s for this RadioButton. You can create and pass in your own remembered
- * [MutableInteractionSource] if you want to observe [Interaction]s and customize the
- * appearance / behavior of this RadioButton in different [Interaction]s.
- * @param contentSide The side where we want to show the label, default to [ContentSide.End].
- * @param content The content displayed after the radio button, usually a Text composable shown at the end.
- */
-@Deprecated(
-    message = "Intent RadioButtonLabelled have been deprecated in favour of just an error parameter",
-    replaceWith = ReplaceWith(
-        "RadioButtonLabelled(selected = selected, onClick = onClick, modifier = modifier, enabled = enabled, error = false, interactionSource = interactionSource, contentSide = contentSide, content = content)",
-    ),
-)
-@Composable
-public fun RadioButtonLabelled(
-    selected: Boolean,
-    intent: ToggleIntent,
-    onClick: (() -> Unit)?,
-    modifier: Modifier = Modifier,
-    verticalAlignment: Alignment.Vertical = Alignment.Top,
-    enabled: Boolean = true,
-    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
-    contentSide: ContentSide = ContentSide.End,
-    content: @Composable RowScope.() -> Unit,
-) {
-    SparkToggleLabelledContainer(
-        state = ToggleableState(selected),
-        toggle = {
-            RadioButton(
-                modifier = it.align(
-                    verticalAlignment,
-                ),
-                intent = intent,
-                selected = selected,
-                onClick = null,
-                interactionSource = interactionSource,
-                enabled = enabled,
-            )
-        },
-        role = Role.RadioButton,
-        onClick = onClick,
-        modifier = modifier,
-        enabled = enabled,
-        contentSide = contentSide,
-        content = content,
     )
 }
 
@@ -248,7 +133,6 @@ public fun RadioButtonLabelled(
  * [Interaction]s for this RadioButton. You can create and pass in your own remembered
  * [MutableInteractionSource] if you want to observe [Interaction]s and customize the
  * appearance / behavior of this RadioButton in different [Interaction]s.
- * @param contentSide The side where we want to show the label, default to [ContentSide.End].
  * @param content The content displayed after the radio button, usually a Text composable shown at the end.
  */
 @Composable
@@ -260,7 +144,6 @@ public fun RadioButtonLabelled(
     enabled: Boolean = true,
     error: Boolean = false,
     interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
-    contentSide: ContentSide = ContentSide.End,
     content: @Composable RowScope.() -> Unit,
 ) {
     SparkToggleLabelledContainer(
@@ -281,7 +164,6 @@ public fun RadioButtonLabelled(
         onClick = onClick,
         modifier = modifier,
         enabled = enabled,
-        contentSide = contentSide,
         content = content,
     )
 }
