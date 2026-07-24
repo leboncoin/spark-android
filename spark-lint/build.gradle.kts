18a88338320a49c05ea6cfc8e64d09f060d4011a
Simon Marquis <simon.marquis@adevinta.com>
2026-07-24 09:02:03 +0200

    Update Java and Kotlin compatibility to version 17

--- spark-lint/build.gradle.kts
+++ spark-lint/build.gradle.kts
@@ -24,6 +24,13 @@ plugins {
     alias(libs.plugins.spark.lint)
 }
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+kotlin.compilerOptions.jvmTarget = JvmTarget.JVM_17
+
 dependencies {
     compileOnly(libs.kotlin.stdlib)
     compileOnly(libs.lint.api)
