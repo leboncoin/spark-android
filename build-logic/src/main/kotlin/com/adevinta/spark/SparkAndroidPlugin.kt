636ffc794bae49eebdb4290509a5a53193e615fa
Scott Rayapoullé <scott.rayapoulle@adevinta.com>
2026-09-14 22:51:06 +0200

    fix(build): set compileSdkMinor=1 for alpha Compose 1.13.0-alpha03

--- build-logic/src/main/kotlin/com/adevinta/spark/SparkAndroidPlugin.kt
+++ build-logic/src/main/kotlin/com/adevinta/spark/SparkAndroidPlugin.kt
@@ -34,6 +34,7 @@ public class SparkAndroidPlugin : Plugin<Project> {
             configureAndroid {
                 compileSdk = spark().versions.compileSdk.toString().toInt()
                 compileSdkExtension = 1
+                compileSdkMinor = 1
                 defaultConfig.minSdk = spark().versions.minCompileSdk.toString().toInt()
                 packaging.apply {
                     resources {
